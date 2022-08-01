87c8ded107fa7d5e89a30cca6603b2b4521e4d40
Thomas Hoang <megatunger@gmail.com>
2022-08-01 16:20:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ sudo apt install git
 
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
+source $HOME/.poetry/env
+
 poetry install
 
 python3 manage.py migrate
